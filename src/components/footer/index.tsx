437db5ebf4885d511663c9d15152770cd374825f
nguyendinhdung <dungnd@tienngay.vn>
2023-01-16 17:04:11 +0700

    update footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,7 +5,7 @@ import IcYT from 'assets/icon/ic_youtube.svg';
 import IcFR from 'assets/icon/ic_frame.svg';
 import IcPublic from 'assets/icon/ic_public.svg';
 import classNames from 'classnames/bind';
-import Languages from 'commons/languages'; 
+import Languages from 'commons/languages';
 import { Button } from 'components/button';
 import { BUTTON_STYLES } from 'components/button/types';
 import { MyTextInput } from 'components/input/index';
@@ -14,6 +14,8 @@ import React, { memo, useCallback, useMemo, useRef, useState } from 'react';
 import styles from './footer.module.scss';
 import { TextFieldActions } from 'components/input/types';
 import { TYPE_INPUT } from 'commons/constants';
+import formValidate from 'utils/form-validate';
+import toasty from 'utils/toasty';
 
 const cx = classNames.bind(styles);
 
@@ -34,39 +36,62 @@ function Footer() {
         note: ''
     });
 
-    const onSend = () => {
-        // api call
-        refName.current?.setValue?.('');
-        refPhone.current?.setValue(''); 
-        refNote.current?.setValue(''); 
-        setFormFeedback({
-            user: '',
-            phone: '',
-            note: ''
-        });    
-    };
-
-    const renderButton = useCallback((_label: string) => {
-        return (
-            <Button
-                label={_label}
-                buttonStyle={BUTTON_STYLES.OUTLINE_GREEN}
-                width={100}
-                containButtonStyles={cx('btn-footer-style')}
-                onPress={onSend}
-            />
+    const onSend = useCallback(() => {
+        const errMsgPhone = formValidate.passConFirmPhone(
+            refPhone.current?.getValue()
         );
+        const errMsgName = formValidate.userNameValidate(
+            refName.current?.getValue()
+        );
+
+        if (!refName.current?.getValue()) {
+            toasty.error(errMsgName);
+        } else if (!refPhone.current?.getValue()) {
+            toasty.error(errMsgPhone);
+        }
+
+        if (errMsgPhone.length + errMsgName.length > 0) {
+            refName.current?.setValue?.('');
+            refPhone.current?.setValue('');
+            refNote.current?.setValue('');
+            setFormFeedback({
+                user: '',
+                phone: '',
+                note: ''
+            });
+        }
     }, []);
 
+    const renderButton = useCallback(
+        (_label: string) => {
+            return (
+                <Button
+                    label={_label}
+                    buttonStyle={BUTTON_STYLES.OUTLINE_GREEN}
+                    width={100}
+                    containButtonStyles={cx('btn-footer-style')}
+                    onPress={onSend}
+                />
+            );
+        },
+        [onSend]
+    );
+
     const renderInput = useCallback(
-        (_ref: any, _value: any, _type: string, _placeholder: string) => {
+        (
+            _ref: any,
+            _value: any,
+            _type: string,
+            _placeholder: string,
+            _maxLength: number
+        ) => {
             const onChange = (e: string) => {
                 switch (_placeholder) {
                     case Languages.footer.yourName:
-                        setFormFeedback({...formFeedback, user: e});
+                        setFormFeedback({ ...formFeedback, user: e });
                         break;
                     case Languages.footer.phoneNumber:
-                        setFormFeedback({...formFeedback, phone: e});
+                        setFormFeedback({ ...formFeedback, phone: e });
                         break;
                     default:
                         break;
@@ -81,42 +106,53 @@ function Footer() {
                     inputStyle={cx('style-input')}
                     containerInput={cx('ctn-style-input')}
                     onChangeText={onChange}
+                    maxLength={_maxLength}
                 />
             );
-        }, [formFeedback]
+        },
+        [formFeedback]
     );
 
-    const renderTextarea = useCallback((_ref:any, _placeholder: string) => {
-        const onChange = (e: string) => {
-            setFormFeedback({...formFeedback, note: e});
-        };
-        return (
-            <MyTextAreaInput
-                ref={_ref}
-                placeHolder={_placeholder}
-                inputStyle={cx('textarea-footer')}
-                containerInput={cx('textarea-container')}
-                value={formFeedback.note}
-                onChangeText={onChange}
-            />
-        );
-    }, [formFeedback]);
+    const renderTextarea = useCallback(
+        (_ref: any, _placeholder: string) => {
+            const onChange = (e: string) => {
+                setFormFeedback({ ...formFeedback, note: e });
+            };
+            return (
+                <MyTextAreaInput
+                    ref={_ref}
+                    placeHolder={_placeholder}
+                    inputStyle={cx('textarea-footer')}
+                    containerInput={cx('textarea-container')}
+                    value={formFeedback.note}
+                    onChangeText={onChange}
+                />
+            );
+        },
+        [formFeedback]
+    );
 
     const renderFooter = useMemo(() => {
         return (
             <div className={cx('text-left')}>
                 <div className={cx('box-footer-top')}>
                     <Row>
-                        <Col span={9} xs={24}  md={24} lg={8}>
+                        <Col span={9} xs={24} md={24} lg={8}>
                             <div className={cx('box-footer-left pb16 ')}>
-                                <div className={cx('content-footer-left', 'column g-15')}>
+                                <div className={cx('content-footer-left', 'column g-10')}>
                                     <img src={IcLogo} className={cx('icon-tienngay')} />
-                                    <div className={cx('h5 medium text-white-style')}>{Languages.footer.companyName}</div>
+                                    <div className={cx('h5 medium text-white-style')}>
+                                        {Languages.footer.companyName}
+                                    </div>
                                     <span className={cx('h6 text-white-style', 'company')}>
                                         {Languages.footer.companyAddress}
                                     </span>
-                                    <span className={cx('h6 text-white-style')}>{Languages.footer.email}</span>
-                                    <span className={cx('h6 text-white-style')}>{Languages.footer.phone}</span>
+                                    <span className={cx('h6 text-white-style')}>
+                                        {Languages.footer.email}
+                                    </span>
+                                    <span className={cx('h6 text-white-style')}>
+                                        {Languages.footer.phone}
+                                    </span>
                                     <div className={cx('footer-icon', 'jus-start g-16')}>
                                         <img src={IcFB} alt="" />
                                         <img src={IcFR} alt="" />
@@ -126,37 +162,67 @@ function Footer() {
                                 </div>
                             </div>
                         </Col>
-                        <Col span={6} xs={24} md={12} lg={8} >
-                            <div className={cx('box-information', 'column g-24 center-flex-left pb16')}>
-                                <div className={cx('information', 'column g-8 text-white-style')}>
+                        <Col span={6} xs={24} md={12} lg={8}>
+                            <div
+                                className={cx(
+                                    'box-information',
+                                    'column g-24 center-flex-left pb16'
+                                )}
+                            >
+                                <div
+                                    className={cx('information', 'column g-8 text-white-style')}
+                                >
                                     <div className={cx('h5 medium text-white-style ')}>
                                         {Languages.footer.information}
                                     </div>
-                                    {Languages.footer.informationChild.map((item)=><span className={cx('h6')} key={item}> 
-                                        {item} 
-                                    </span>)}
+                                    {Languages.footer.informationChild.map((item) => (
+                                        <span className={cx('h6')} key={item}>
+                                            {item}
+                                        </span>
+                                    ))}
                                 </div>
-                                <div className={cx('information', 'column g-8 text-white-style')}>
+                                <div
+                                    className={cx('information', 'column g-8 text-white-style')}
+                                >
                                     <div className={cx('h5 medium text-white-style')}>
                                         {Languages.footer.customerSupport}
                                     </div>
-                                    {Languages.footer.customerChild.map((item)=><span className={cx('h6 text-white-style')} key={item}>
-                                        {item}
-                                    </span>)}
+                                    {Languages.footer.customerChild.map((item) => (
+                                        <span className={cx('h6 text-white-style')} key={item}>
+                                            {item}
+                                        </span>
+                                    ))}
                                 </div>
                             </div>
                         </Col>
-                        <Col span={9} xs={24} md={12} lg={8} >
-                            <div className={cx('box-footer-right', 'column g-8 text-white-style')}>
+                        <Col span={9} xs={24} md={12} lg={8}>
+                            <div
+                                className={cx(
+                                    'box-footer-right',
+                                    'column g-8 text-white-style'
+                                )}
+                            >
                                 <div className={cx('h5 medium text-white-style')}>
                                     {Languages.footer.customerFeedback}
                                 </div>
                                 <Row className={cx('space-between')}>
                                     <Col span={12}>
-                                        {renderInput(refName, formFeedback.user, TYPE_INPUT.TEXT, Languages.footer.yourName)}
+                                        {renderInput(
+                                            refName,
+                                            formFeedback.user,
+                                            TYPE_INPUT.TEXT,
+                                            Languages.footer.yourName,
+                                            50
+                                        )}
                                     </Col>
                                     <Col span={11}>
-                                        {renderInput( refPhone, formFeedback.phone, TYPE_INPUT.TEL, Languages.footer.phoneNumber)}
+                                        {renderInput(
+                                            refPhone,
+                                            formFeedback.phone,
+                                            TYPE_INPUT.PHONE,
+                                            Languages.footer.phoneNumber,
+                                            10
+                                        )}
                                     </Col>
                                 </Row>
                                 <Row>
@@ -174,11 +240,19 @@ function Footer() {
                     </Row>
                 </div>
                 <div className={cx('box-footer-bottom')}>
-                    <span className={cx('h6 text-white-style')}>{Languages.footer.copyRight}</span>
+                    <span className={cx('h6 text-white-style')}>
+                        {Languages.footer.copyRight}
+                    </span>
                 </div>
             </div>
         );
-    }, [formFeedback.phone, formFeedback.user, renderButton, renderInput, renderTextarea]);
+    }, [
+        formFeedback.phone,
+        formFeedback.user,
+        renderButton,
+        renderInput,
+        renderTextarea
+    ]);
     return <div className={cx('footer-container')}>{renderFooter}</div>;
 }
 
